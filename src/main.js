0ede82bcc6c8dfbf3381be6f656671455f200680
Devon Bacon <devon.bacon@gmail.com>
2020-01-31 16:24:58 -0800

    try todo-item component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+/* Vue.component("test-template", {
+  template: "#testTemplate",
+  props: ["testValue"]
+}); */
+
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>{{ todo.text }}</li>"
+});
+
 new Vue({
   render: h => h(App)
 }).$mount("#app");
